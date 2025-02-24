2df0e6c97d303751140e3c2e1868f987e6dcb560
“macarthuror” <arturo.ortegaro@gmail.com>
2025-02-23 20:57:43 -0600

    🚧🏗️🧱 Migrate to AdonisJS

--- src/config/index.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config()
-
-import server from './server.config';
-export { config as db } from './database.config'
-
-export default server;
--- tests/demo.test.ts
+++
@@ -1,5 +0,0 @@
-describe('demo test', () => {
-    test('demo', () => {
-        expect(true).toBe(true)
-    })
-})
