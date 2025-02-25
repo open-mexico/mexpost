1e332e22ae6bca6ef39ad80465831d7b79527ff3
Arturo Ortega <24395219+macarthuror@users.noreply.github.com>
2025-02-24 19:32:33 -0600

    👷Vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,8 @@
+export default {
+  lang: 'es-MX',
+  title: 'Mexpost |   Open México',
+  titleTemplate: '- Mexpost',
+  description: 'Base de datos de códigos postales de México',
+
+  base: '/mexpost/',
+}
